d66b76b6c9dbe22fe038ea10e5e12adff1111ba5
Facundo Esposito <espositofacundo92@gmail.com>
2024-05-11 13:07:05 -0300

    restaurar carpeta category

--- src/app/(shop)/category/not-found.tsx
+++ src/app/(shop)/category/not-found.tsx
@@ -1,22 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import PageNotFound from "@/components/ui/not-found/PageNotFound";
 
-const PageNotFound = () => {
+
+export default function PageNotFoundCategory() {
   return (
-    <div className="flex flex-col-reverse md:flex-row h-[800px] w-full justify-center items-center align-middle">
-      <div className="text-center">
-      <Link href="/" className="text-blue-800 text-2xl font-bold">Regresar</Link>
-        <h1 className="text-4xl">Error 404 not found</h1>
-        
-      </div>
-
-      <div>
-        <Image src="/imgs/starman_750x750.png" alt="startman" className="p-5 sm:p-0" width={550} height={550}/>
-      </div>
-    
-    </div>
+    <PageNotFound/>
   );
-};
-
-export default PageNotFound;
+}
